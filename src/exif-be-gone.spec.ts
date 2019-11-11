42a299de409d0d4acca27c17f9d6559875ed2569
Vico <victor.loizel@netc.fr>
2019-11-11 17:17:58 +0100

    Refactor and add remove function to remove exif data and write file

--- src/exif-be-gone.spec.ts
+++ src/exif-be-gone.spec.ts
@@ -1,12 +1,13 @@
 import {WritableStreamBuffer} from 'stream-buffers';
 import {ExifBeGone} from './index';
 import * as fs from 'fs';
+import {PATH_TEST_FILE_IN} from '../testing/constants';
 
 describe('Exif be gone', () => {
 
   it('should strip data', async () => {
     const writer: WritableStreamBuffer = new WritableStreamBuffer();
-    await fs.createReadStream('Canon_40D.jpg')
+    await fs.createReadStream(PATH_TEST_FILE_IN)
         .pipe(new ExifBeGone())
         .pipe(writer)
         .on('finish', () => {
